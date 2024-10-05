8bd5135fca1342ff188683a5cabde76186334acd
1221solomon-tadesse <solomontadesse480@gmail.com>
2024-10-05 10:25:55 +0300

    feat(fix): updated Navbar directory

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 
-import Navbar from '@/components/ui/Navbar';
+import HeroSection from '@/components/ui/HeroSection';
+import Navbar from '@/components/presentation/Navbar';
 export default function Home() {
   return (
     <div>
-     
       <Navbar/>
+      <HeroSection/>
     </div>
   );
 }
