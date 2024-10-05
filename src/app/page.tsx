24a61462749626a24fe307b344c6d0f06cf17b1f
1221solomon-tadesse <solomontadesse480@gmail.com>
2024-10-05 11:01:03 +0300

    feat(fix) ..

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
 
-import HeroSection from '@/components/ui/HeroSection';
 import Navbar from '@/components/presentation/Navbar';
 export default function Home() {
   return (
     <div>
       <Navbar/>
-      <HeroSection/>
+     
     </div>
   );
 }
